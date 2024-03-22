9a2eadb508a5ad11cc46ff23360802837e473c4e
harunsaleh <harunsaleh1207@gmail.com>
2024-03-22 13:00:03 +0100

    commit

--- Lab_4.py
+++ Lab_4.py
@@ -1,4 +1,8 @@
 def calculate_average():
-    input = int("Please enter at least five test scores:")
-    letters = [input]
+    a = int("Please enter at least five test scores:")
+    b = int("Please enter at least five test scores:")
+    c = int("Please enter at least five test scores:")
+    d = int("Please enter at least five test scores:")
+    e = int("Please enter at least five test scores:")
+    letters = [a,b,c,d,e]
     print(letters)
